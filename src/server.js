b10c31dff8a23318e77b6d44b69b486a5d72a4bf
sathyasathyabc-png <sathya.sathya.bc@gmail.com>
2026-05-15 19:49:17 +0530

    onboarding new files

---
+++ src/server.js
@@ -0,0 +1,14 @@
+// server.js
+const express = require('express');
+const app = express();
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello, world!' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
---
+++ src/server.test.js
@@ -0,0 +1,11 @@
+// server.test.js
+const request = require('supertest');
+const app = require('./server');
+
+describe('GET /api/hello', () => {
+  it('should return Hello, world! message', async () => {
+    const res = await request(app).get('/api/hello');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.message).toBe('Hello, world!');
+  });
+});
